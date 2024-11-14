e38698af0d501b187567d004b43c0fff02db7cd6
arsenius <gamerpop25@gmail.com>
2024-11-14 22:44:38 +0400

    Add CategoryListComponent

---
+++ src/components/CategoryListComponent.js
@@ -0,0 +1,83 @@
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+
+const CategoryListComponent = () => {
+    // Список категорий
+    const [categoryList, setCategoryList] = useState([]);
+
+    // Обновить токен
+    const UpdateToken = async () => {
+        try{
+            const response = await axios.get("https://localhost:7777/api/token/update", {
+                headers:{
+                    "Authorization": "Bearer " + localStorage.getItem("token")
+                }
+            });
+
+            if(response.status === 200)
+            {
+                const authToken = response.data.token;
+                if(authToken)
+                {
+                    const token = authToken.replace("Bearer");
+                    localStorage.setItem(token);
+                }
+            }
+        }
+        catch(error)
+        {
+            // Внутрянняя ошибка сервера (Internal server error)
+            if(error.response && error.response.status === 500)
+                console.log(error);
+
+            // Not Found
+            else if(error.response && error.response.status === 404)
+                console.log(error);
+
+            else
+                console.log(error);
+        }
+    }
+
+    // Получить список категорий 
+    const GetCategoryListAsync = async () => {
+        try{
+            const response = await axios.get("https://localhost:7299/api/category");
+
+            if(response.status === 200)
+            {
+                const responseData = response.data.categoryList;
+                if(response.data.categoryList)
+                {
+                    setCategoryList(responseData);
+                }
+            }
+        }
+        catch(error)
+        {
+            // Если токен истёк или не зарегистировался/вошёл
+            if(error.response && error.response.status === 401)
+                await UpdateToken();
+        }
+    }
+
+    useEffect(() => {
+        GetCategoryListAsync();
+      }, []);
+
+    return (
+        <div>
+            <ul>
+                {categoryList.map((category, index) => (
+                    <li key={index}>    
+                        <div>
+                           <p>{category.name}</p>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default CategoryListComponent
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,29 @@
 import React from "react";
 import HandleCategoryComponent from "./HandleComponents/HandleCategoryComponent";
+import CategoryListComponent from "./CategoryListComponent";
 
 const Home = () => {
     return (
         <div>
-            Главная страница
-            <HandleCategoryComponent/>
+            <div>
+                <h3>Главная страница</h3>
+            </div>
+            <div>
+                <div>
+                    <p>Список категорий</p>
+                </div>
+                <div>
+                    <CategoryListComponent/>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <p>Управление категориями</p>
+                </div>
+                <div>
+                    <HandleCategoryComponent/>
+                </div>
+            </div>
         </div>
     );
 };
